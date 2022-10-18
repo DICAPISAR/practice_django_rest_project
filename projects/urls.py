cca987e8b26c6ea5ca5f2f3cb5bcc987b6761925
dicapisar <dicapisar@gmail.com>
2022-10-17 19:59:27 -0500

    creation base of project with django framework rest

---
+++ projects/urls.py
@@ -0,0 +1,9 @@
+from django.urls import URLPattern
+from rest_framework import routers
+from projects.api import ProjectViewSet
+
+router = routers.DefaultRouter()
+
+router.register('api/projects', ProjectViewSet, 'projects')
+
+urlpatterns = router.urls
